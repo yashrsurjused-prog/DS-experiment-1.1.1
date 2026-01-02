5e5de5bfbd64647de9da2582ecafb3033bd93890
Yash Surjuse <yashrsurjused@gmail.com>
2026-01-02 15:06:19 +0530

    Add files via upload

---
+++ exp1.1.1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main() {
+	int n,arr[100],key, pos, flag = 0;
+
+	scanf("%d",&n);
+
+	for(int i=0; i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+scanf("%d", &key);
+	for (int i=0; i<n;i++){
+		if(arr[i]==key){
+			pos = i;
+			flag= 1;
+			break;
+		}
+	
+	
+	
+	
+	//Type your content here
+	
+	
+	
+	
+	}
+	if (flag == 1) {
+		printf("found at position %d\n", pos);
+	} else {
+		printf("%d not found\n", key);
+	}
+}
